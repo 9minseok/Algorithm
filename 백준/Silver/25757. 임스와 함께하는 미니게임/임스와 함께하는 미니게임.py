c420bf720827ca083998dc84c82f2dc8d5fe741d
9minseok <118731422+9minseok@users.noreply.github.com>
2023-11-22 16:14:34 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 3740 ms, Memory: 42388 KB -BaekjoonHub

---
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.py
@@ -0,0 +1,12 @@
+N, game = map(str, input().split())
+
+game_list = {'Y':2, 'F':3, 'O':4}
+game_cnt = game_list[game]
+
+set_members = set()
+
+for _ in range(int(N)):
+  set_members.add(input())
+
+arr_member = list(set_members)
+print(len(arr_member)//(game_cnt - 1))
