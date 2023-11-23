e3a629f56681f25ebfbe0d3413e360d783f50ddb
9minseok <118731422+9minseok@users.noreply.github.com>
2023-11-23 22:10:52 +0900

    [Bronze I] Title: 피보나치 수, Time: 120 ms, Memory: 10596 KB -BaekjoonHub

---
+++ 백준/Bronze/4150. 피보나치 수/피보나치 수.js
@@ -0,0 +1,15 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+let count = Number(input[0]);
+
+let fibonacci = [0, 1];
+
+let sum = 0;
+
+for (let i = 1; i <= count; i++) {
+    sum = BigInt(fibonacci[0]) + BigInt(fibonacci[1])
+    fibonacci.shift();
+    fibonacci.push(sum)
+}
+
+console.log(fibonacci[0].toString());
