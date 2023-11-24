31d2745ddd19ba6b80e2092bed4482765000a523
9minseok <118731422+9minseok@users.noreply.github.com>
2023-11-24 13:41:24 +0900

    [Silver IV] Title: 문자열 집합, Time: 204 ms, Memory: 39040 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.js
@@ -0,0 +1,17 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [N, M] = input[0].split(' ').map(Number);
+const N_data = input.slice(1, 1 + N);
+const M_data = input.slice(1 + N);
+
+let count = 0;
+
+const resultObject = {};
+
+N_data.forEach((ele) => (resultObject[ele] = true));
+
+M_data.forEach((ele) => {
+  if (resultObject[ele]) count++;
+}); 
+
+console.log(count)
