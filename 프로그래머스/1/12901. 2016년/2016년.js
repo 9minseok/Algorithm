d341a55e0d4c92f723a8f19f64954a6522fe94ab
9minseok <118731422+9minseok@users.noreply.github.com>
2023-11-27 22:21:04 +0900

    [level 1] Title: 2016년, Time: 0.11 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -0,0 +1,7 @@
+function solution(a, b) {
+    let week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+    let dateStr = '2016-' + a + '- ' + b;
+    let date = new Date(dateStr);
+    
+    return week[date.getDay()];
+}
